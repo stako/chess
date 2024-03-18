464d3ee01b20c3173f07687208484da1c25a580a
stako <stako@users.noreply.github.com>
2024-03-18 16:23:31 -0700

    add #place_piece(s) methods to Game class

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -5,7 +5,6 @@ module Chess
     def initialize
       @board = Array.new(8) { Array.new(8) }
       @highlighted_tiles = nil
-      place_pieces
     end
 
     def [](index)
@@ -33,32 +32,6 @@ module Chess
       else
         alt ? piece.on_grey : piece
       end
-    end
-
-    def place_piece(type, color, file, rank)
-      @board[file][rank] = type.new(color, [file, rank])
-    end
-
-    def place_pieces
-      place_piece(Rook, WHITE, 0, 0)
-      place_piece(Rook, WHITE, 7, 0)
-      place_piece(Knight, WHITE, 1, 0)
-      place_piece(Knight, WHITE, 6, 0)
-      place_piece(Bishop, WHITE, 2, 0)
-      place_piece(Bishop, WHITE, 5, 0)
-      place_piece(Queen, WHITE, 3, 0)
-      place_piece(King, WHITE, 4, 0)
-      8.times { |x| place_piece(Pawn, WHITE, x, 1) }
-
-      place_piece(Rook, BLACK, 0, 7)
-      place_piece(Rook, BLACK, 7, 7)
-      place_piece(Knight, BLACK, 1, 7)
-      place_piece(Knight, BLACK, 6, 7)
-      place_piece(Bishop, BLACK, 2, 7)
-      place_piece(Bishop, BLACK, 5, 7)
-      place_piece(Queen, BLACK, 3, 7)
-      place_piece(King, BLACK, 4, 7)
-      8.times { |x| place_piece(Pawn, BLACK, x, 6) }
     end
   end
 end
--- lib/chess/game.rb
+++ lib/chess/game.rb
@@ -6,6 +6,33 @@ module Chess
 
     def initialize
       @board = Board.new
+      place_pieces
+    end
+
+    def place_piece(type, color, file, rank)
+      @board[file][rank] = type.new(color, [file, rank])
+    end
+
+    def place_pieces
+      place_piece(Rook, WHITE, 0, 0)
+      place_piece(Rook, WHITE, 7, 0)
+      place_piece(Knight, WHITE, 1, 0)
+      place_piece(Knight, WHITE, 6, 0)
+      place_piece(Bishop, WHITE, 2, 0)
+      place_piece(Bishop, WHITE, 5, 0)
+      place_piece(Queen, WHITE, 3, 0)
+      place_piece(King, WHITE, 4, 0)
+      8.times { |x| place_piece(Pawn, WHITE, x, 1) }
+
+      place_piece(Rook, BLACK, 0, 7)
+      place_piece(Rook, BLACK, 7, 7)
+      place_piece(Knight, BLACK, 1, 7)
+      place_piece(Knight, BLACK, 6, 7)
+      place_piece(Bishop, BLACK, 2, 7)
+      place_piece(Bishop, BLACK, 5, 7)
+      place_piece(Queen, BLACK, 3, 7)
+      place_piece(King, BLACK, 4, 7)
+      8.times { |x| place_piece(Pawn, BLACK, x, 6) }
     end
   end
 end
