c23a4f0b7dc636bb5e5f32f427e7c5d5f778ffc4
stako <stako@users.noreply.github.com>
2024-03-22 16:50:53 -0700

    add notation display to #print_board

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -5,14 +5,17 @@ module Chess
     attr_reader :board
 
     def print_board(selected_piece = nil)
-      display = ""
+      file_text = "  a b c d e f g h\n"
+      display = file_text.dup
       (0..7).reverse_each do |rank|
+        display << "#{rank + 1} "
         8.times do |file|
           piece = @board[file][rank]&.to_s || EMPTY
           display << color(piece, file, rank, selected_piece&.move_list)
         end
-        display << "\n"
+        display << " #{rank + 1}\n"
       end
+      display << file_text
       puts display
     end
 
