487471d44867a97b11f1b1db5c491f55395e72c4
stako <stako@users.noreply.github.com>
2024-04-17 19:58:45 -0700

    feat: add position class

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 require_relative "chess/player"
+require_relative "chess/position"
---
+++ lib/chess/position.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Chess
+  Position = Data.define(:row, :column)
+end
