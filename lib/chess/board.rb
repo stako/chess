64e69d4f7105bcd0c594dde3827b1690f9c595d8
stako <stako@users.noreply.github.com>
2024-04-17 22:08:46 -0700

    feat(Board): add copy method

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -12,6 +12,13 @@ module Chess
       board
     end
 
+    def copy
+      new_board = self.class.new
+
+      piece_positions.each { |pos| new_board[pos] = self[pos].copy }
+      new_board
+    end
+
     # usage:
     # board[row, column] where row & column are integers
     # board[pos] where pos is a Position object
@@ -51,6 +58,17 @@ module Chess
         self[1, column] = Pawn.new(Player::BLACK)
         self[6, column] = Pawn.new(Player::WHITE)
       end
+    end
+
+    def piece_positions
+      array = []
+      8.times do |row|
+        8.times do |column|
+          pos = Position.new(row, column)
+          array << pos unless empty?(pos)
+        end
+      end
+      array
     end
   end
 end
