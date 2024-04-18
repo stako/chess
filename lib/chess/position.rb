9dadb480f0079404433e1f94df1eb536cb4ff080
stako <stako@users.noreply.github.com>
2024-04-17 20:16:49 -0700

    feat: allow a Direction to be added to a Position

--- lib/chess/position.rb
+++ lib/chess/position.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 module Chess
-  Position = Data.define(:row, :column)
+  Position = Data.define(:row, :column) do
+    # 'other' must be a Direction object
+    def +(other)
+      self.class.new(row + other.row_delta, column + other.column_delta)
+    end
+  end
 end
