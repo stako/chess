5d5d651785d6c46f25ad73e1ee94647c5990bcf7
stako <stako@users.noreply.github.com>
2024-04-22 15:42:04 -0700

    feat: add Game class

---
+++ lib/chess/game.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Chess
+  class Game
+    attr_reader :board
+    attr_accessor :current_player
+
+    def initialize(player, board)
+      @current_player = player
+      @board = board
+    end
+
+    def legal_moves_for_piece(pos)
+      return [] if board.empty?(pos) || board[pos].color != current_player
+
+      piece = board[pos]
+      piece.get_moves(pos, board).select { |move| move.legal?(board) }
+    end
+
+    def all_legal_moves_for(player)
+      board.piece_positions_for(player).flat_map { |pos| legal_moves_for_piece(pos) }
+    end
+
+    def game_over?(player)
+      all_legal_moves_for(player).empty?
+    end
+
+    def make_move(move)
+      move.execute(board)
+      self.current_player = Player.opponent(current_player)
+    end
+  end
+end
